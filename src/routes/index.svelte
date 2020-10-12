6e068caad41d115a1d25b668c78325016806df03
Onur Orhan <onur@houseofapps.com>
2020-10-12 10:52:15 +0300

    carousel content

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,49 +1,3 @@
-<!-- <style>
-	h1, figure, p {
-		text-align: center;
-		margin: 0 auto;
-	}
-
-	h1 {
-		font-size: 2.8em;
-		text-transform: uppercase;
-		font-weight: 700;
-		margin: 0 0 0.5em 0;
-	}
-
-	figure {
-		margin: 0 0 1em 0;
-	}
-
-	img {
-		width: 100%;
-		max-width: 400px;
-		margin: 0 0 1em 0;
-	}
-
-	p {
-		margin: 1em auto;
-	}
-
-	@media (min-width: 480px) {
-		h1 {
-			font-size: 4em;
-		}
-	}
-</style>
-
-<svelte:head>
-	<title>Sapper project template</title>
-</svelte:head>
-
-<h1>Great success!</h1>
-
-<figure>
-	<img alt='Success Kid' src='successkid.jpg'>
-	<figcaption>Have fun with Sapper!</figcaption>
-</figure>
-
-<p><strong>Try editing this file (src/routes/index.svelte) to test live reloading.</strong></p> -->
 
 <svelte:head>
   <title>Sapper project template</title>
@@ -62,6 +16,8 @@
 <script>
 	export let dashboard;
 
+	console.log('dashboard', dashboard);
+
 </script>
 
 <style>
@@ -72,6 +28,8 @@
 
 
 {@html dashboard[0].html}
+
+{@html dashboard[1].html}
 
 <h1>dashboard</h1>
 
